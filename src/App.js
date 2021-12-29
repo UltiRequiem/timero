7d964dedb7a7adc10fab86a65a687b1d80c98049
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-12-29 08:36:08 -0500

    :tada: Begin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import timeZones from "./timeZones";
 
-function App() {
+export default function App() {
+  const [timeZone, setTimeZone] = useState("America/Lima");
+
+  const parseTime = (date) => {
+    try {
+      return date
+        .toLocaleString("en-US", {
+          timeZone,
+        })
+        .split(",");
+    } catch {
+      return ["", ""];
+    }
+  };
+
+  const [time, setTime] = useState(parseTime(new Date()));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(parseTime(new Date()));
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [parseTime]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <h1>Timero</h1>
+      <main>
+        <div className="input">
+          <label htmlFor="timeZoneInput">Put your TimeZone</label>
+          <input
+            htmlFor="timeZoneInput"
+            value={timeZone}
+            onInput={(event) => {
+              setTimeZone(event.target.value);
+            }}
+          />
+        </div>
+
+        <p>{time[0]}</p>
+        <p>{time[1]}</p>
+      </main>
+
+      <footer>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <a href="https://github.com/UltiRequiem">
+            Eliaz Bobadilla (a.k.a UltiRequiem)
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </footer>
+    </>
   );
 }
-
-export default App;

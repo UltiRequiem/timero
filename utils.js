047f3709455f0d06549991821d04096deca008d5
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-01-08 07:07:47 -0500

    :art: Format comments

--- utils.js
+++ utils.js
@@ -1,28 +1,27 @@
-import timeZones from "./timezones.js";
-import {Fuse} from "./deps.js";
+import tz from "./timezones.js";
+import { Fuse } from "./deps.js";
 
-const fuse = new Fuse(timeZones);
+const fuse = new Fuse(tz);
 /**
  * @param {string} customTZ The input timezone
  * @returns {string} The fuzzy founded Time Zone
  */
-
 export function findSimilarTZ(customTZ) {
   return fuse.search(customTZ)[0].item;
 }
+
 /**
  * @param {Date} date
  * @param {string} timeZone
  */
-
 export function getDateHourFormatted(date, timeZone) {
-  return date.toLocaleString("en-US", {timeZone}).split(",");
+  return date.toLocaleString("en-US", { timeZone }).split(",");
 }
+
 /**
  * @param {string} input
  * @returns {string}
  */
-
 export function formatInput(input) {
   return input
     .toLowerCase()
@@ -33,13 +32,12 @@ export function formatInput(input) {
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
     .join(" ");
 }
+
 /**
  * @param {string} timezone
  * @param {Date} date
  * @returns
  */
-
 export default function parseInput(timezone, date) {
   return [timezone, ...getDateHourFormatted(timezone, date)];
 }
-

6feb10a16d82aed23c68057d1c3c1687d1bf9433
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-03-18 19:22:13 -0500

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,7 +1,4 @@
 export { default as Fuse } from "https://deno.land/x/fuse@v6.4.1/dist/fuse.esm.js";
 export { default as copy } from "https://esm.sh/copy-to-clipboard";
 
-export {
-  timeZones,
-  randomTimeZone,
-} from "https://deno.land/x/timezones@v1.1.1/mod.ts";
+export { timeZones, randomTimeZone } from "https://deno.land/x/timezones@v1.1.1/mod.ts";
